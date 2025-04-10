4bd4d84ce081caa24ac10a1292d4c55c39b0fabb
mathurninaad <mathurninaad@gmail.com>
2025-04-10 15:46:46 +0530

    Added support for Unused variables detection

--- main.py
+++ main.py
@@ -3,36 +3,54 @@ import re
 
 class MyVisitor(ast.NodeVisitor):
     def __init__(self):
-        self.sast = []
+        # Problems in Python code
+        self.sastP = []
+
+        # unused variables and used variables
+        self.unv = []
+        self.uv = []
+
+
 
     def check_hardcoded(self, target):
         name = target.id.lower()
-        pattern = r"\b\w*(pass(word|wd)?|pwd|psswd)\w*\b"
+        pattern = r"\b\w*(pass(word|wd)?|pwd|psswd)\w*\b" # copied pattern from GPT
 
         if re.search(pattern, name, re.IGNORECASE):
-            self.sast.append((target.lineno, name))
+            self.sastP.append((target.lineno, name))
             return True
         return False
 
-    def visit_For(self, node):
-        print(node.lineno)
-        self.generic_visit(node)
-
     def visit_Assign(self, node):
         for target in node.targets:
-            # Checking if the line has multiple assignments
+            # multiple assignments
             if isinstance(target, ast.Tuple):
                 for elt in target.elts:
+                    self.unv.append((elt.id, elt.lineno))
                     if isinstance(elt, ast.Name):
                         self.check_hardcoded(elt)
             # checking single line assignment
             elif isinstance(target, ast.Name):
+                self.unv.append((target.id, target.lineno))
                 self.check_hardcoded(target)
 
         self.generic_visit(node)
 
+    def visit_Name(self, node):
+        if isinstance(node.ctx, ast.Load):
+            self.uv.append((node.id, node.lineno))
+        self.generic_visit(node)
+
     def printIssues(self):
-        for issue in self.sast:
+        for i in self.unv:
+            found = False
+            for j in self.uv:
+                if j[0] == i[0]:
+                    found =  True
+            if found is False:
+                print(f"Issue on line: {i[1]} [Unused variable]. variable name: {i[0]}")
+
+        for issue in self.sastP:
             print(f"Issue on line: {issue[0]} [Possibility of Hardcoded password]. variable Name: {issue[1]}")
 
 if __name__ == '__main__':
@@ -40,6 +58,6 @@ if __name__ == '__main__':
     with open('test.py') as code:
         code = code.read()
         tree = ast.parse(code)
-        # print(ast.dump(tree, indent=4))
+        print(ast.dump(tree, indent=4))
         visitor.visit(tree)
         visitor.printIssues()
