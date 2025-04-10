e4e852647fcd380a6242acb00d8fe84108e69400
mathurninaad <mathurninaad@gmail.com>
2025-04-10 14:56:16 +0530

    Learning about generic_visit function

--- main.py
+++ main.py
@@ -6,4 +6,12 @@ class MyVisitor(ast.NodeVisitor):
 
     def visit_For(self, node):
         print(node.lineno)
+        self.generic_visit(node) # visit the children nodes of For
 
+
+if __name__ == '__main__':
+    visitor = MyVisitor()
+    with open('test.py') as code:
+        code = code.read()
+        tree = ast.parse(code)
+        visitor.visit(tree)
--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 for i in range(10):
-    print(i)
+    for j in range(20):
+        print(i)
